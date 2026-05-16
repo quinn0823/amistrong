164c12613315fa890ca18134c21a8f9643d1af49
Jonathan Chiu <quinnqiu0823@icloud.com>
2026-05-16 18:03:36 +0800

    Add: Export complete favorable WuXing JSON table.

--- amistrong.py
+++ amistrong.py
@@ -46,8 +46,20 @@ def amistrong(rigan_yuezhi):
 def main():
     rigan = input('日干：')
     yuezhi = input('月支：')
+
     result = amistrong({ 'rigan': rigan, 'yuezhi': yuezhi })
     print(f'{'身强' if result.get('is_strong') else '身弱'}，喜用{''.join(result.get('favorable_wuxing'))}。')
+
+    # rigan_yuezhi_favorable_wuxing_table = {}
+
+    # for rigan in rigan_wuxing_table.keys():
+    #     rigan_yuezhi_favorable_wuxing_table[rigan] = {}
+    #     for yuezhi in yuezhi_wuxing_table.keys():
+    #         result = amistrong({ 'rigan': rigan, 'yuezhi': yuezhi })
+    #         rigan_yuezhi_favorable_wuxing_table[rigan][yuezhi] = result.get('favorable_wuxing')
+
+    # with open(os.path.join(DATA_DIR, 'rigan_yuezhi_favorable_wuxing_table.json'), 'w', encoding='utf-8') as file:
+    #     json.dump(rigan_yuezhi_favorable_wuxing_table, file, ensure_ascii=False, indent=4)
 
 
 if __name__ == '__main__':
